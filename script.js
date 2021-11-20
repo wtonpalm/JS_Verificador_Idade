52f401a385ce3d3e765c25e86b5ed96347141d58
Wellington Palmeira <w-palmeira@hotmail.com>
2021-11-20 15:29:27 -0300

    verificador

---
+++ script.js
@@ -0,0 +1,49 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('#res')
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >= 0 && idade < 12) {
+                //criança
+                img.setAttribute('src', 'imagens/bebe_m.png')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'imagens/jovem_mas.png')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'imagens/adulto.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'imagens/idoso.png')
+            }
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+            if (idade >= 0 && idade < 12) {
+                //criança
+                img.setAttribute('src', 'imagens/bebe_f.png')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'imagens/jovem_fem.png')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'imagens/adulta.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'imagens/idosa.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
